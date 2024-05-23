b98cfd283d79cd1fae52cb97f3eaceb23d171c46
massicheklat <massicheklat@iutnc-503-01.iutnc.site.univ-lorraine.fr>
2024-05-23 16:31:19 +0200

    Debut de jeu 'Morpion'

--- Node.js
+++ Node.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs'); 
+
 app.get('/', (req, res) => {
-res.send('Hello World!');
+res.render('pages/index');
 });
+
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
