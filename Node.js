e388de83a311245d359f0e063af81089cb5e0fe3
massicheklat <massicheklat@iutnc-503-01.iutnc.site.univ-lorraine.fr>
2024-05-27 09:00:44 +0200

    ajout de la foction chekWinner

--- Node.js
+++ Node.js
@@ -1,15 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs'); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+let state = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+]; 
+
+function checkWinner(s){
+    for(let i=0; i<3; i++) {
+        if (state [i][0] === symbol && state [i][1] === symbol && state[i][2] == symbol) {
+            return true
+        }
+}
+    for(let j=0; j<3; i++) {
+        if (state [0][j] === symbol && state [1][j] === symbol && state[2][j] == symbol) {
+            return true
+        }
+    }
+
+    if ((state [0][0] === symbol && state [1][1] === symbol && state[2][2] == symbol) || (state [0][2] === symbol && state [1][1] === symbol && state[2][0] == symbol)) {
+        return true
+    }
+    return false
+}
+
+
+
 
 app.get('/', (req, res) => {
 res.render('pages/index');
 });
 
-app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+app.post('/submit', (req, res) => {
+    const checkboxes = req.body;
+    
 });
 
 
+
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`);
+});
