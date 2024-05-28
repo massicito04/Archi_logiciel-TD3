917ac664f1c2c6e3bc72b4a99f4649f5eb33a135
massicheklat <massicheklat@iutnc-503-01.iutnc.site.univ-lorraine.fr>
2024-05-28 10:16:36 +0200

    Ajout d'un Client Postgres

--- Node.js
+++ Node.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const { Client } = require('pg'); 
 const app = express();
 const port = 3000;
 
@@ -7,7 +8,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const client = new Client ({
+    host: 'localhost', 
+    port: 5432, 
+    user: 'postgres',
+    password: 'bonpoulet',
+    database: 'morpion',   
 
+}); 
 
 let state = [
     ['','',''],
@@ -15,45 +23,43 @@ let state = [
     ['','','']
 ]; 
 
-function checkWinner(s){
+function checkWinner(symbol){
     for(let i=0; i<3; i++) {
-        if (state [i][0] === symbol && state [i][1] === symbol && state[i][2] == symbol) {
-            return true
-        }
-}
-    for(let j=0; j<3; i++) {
-        if (state [0][j] === symbol && state [1][j] === symbol && state[2][j] == symbol) {
-            return true
+        if (state[i][0] == symbol && state[i][1] == symbol && state[i][2] == symbol) {
+            return true;
         }
     }
-
-    if ((state [0][0] === symbol && state [1][1] === symbol && state[2][2] == symbol) || (state [0][2] === symbol && state [1][1] === symbol && state[2][0] == symbol)) {
-        return true
+    for(let j=0; j<3; j++) {
+        if (state[0][j] == symbol && state[1][j] == symbol && state[2][j] == symbol) {
+            return true;
+        }
     }
-    return false
+    if ((state[0][0] == symbol && state[1][1] == symbol && state[2][2] == symbol) || 
+        (state[0][2] == symbol && state[1][1] == symbol && state[2][0] == symbol)) {
+        return true;
+    }
+    return false;
 }
-
-
-
 
 app.get('/', (req, res) => {
-res.render('pages/index');
+    res.render('pages/index', { title: 'Jeu de morpion', message: 'Morpion', state });
 });
 
 app.post('/submit', (req, res) => {
-    const { choice } = req.body
+    const { choice } = req.body;
     const [row, col] = choice.split('-').map(Number); 
-    if(state[row,col] === ''){
-        state[row, col] === 'X'; 
+    if(state[row][col] === ''){
+        state[row][col] = 'X'; 
         if(checkWinner('X')){
-            res.send('Wow, le joueur X à gagner')
+            res.send('Wow, le joueur X à gagner');
             return; 
         } 
     }
+    res.redirect('/');
 });
 
-app.post('/reset', (req)=> {
-    gameState = [
+app.post('/reset', (req,res) => {
+    state = [
         ['','',''],
         ['','',''],
         ['','','']
@@ -61,7 +67,6 @@ app.post('/reset', (req)=> {
     res.redirect('/');
 }); 
 
-
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
